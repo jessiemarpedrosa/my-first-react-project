7edb013322e4b5413d4eb81c48324e71f5568f45
Jessiemar Pedrosa <masie_0119@yahoo.com>
2019-02-13 14:50:32 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import Users from './components/Users';
+import BlogPosts from './components/BlogPosts';
+import axios from 'axios';
+//import ReactPaginate from 'react-paginate';
 
 class App extends Component {
-  render() {
+
+state = {
+  blogPosts: [],
+  blogImages: [],
+  blogAuthors: []
+}
+
+componentDidMount() {
+  axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=15`)
+    .then(res => {
+      const blogPosts = res.data;
+      this.setState({blogPosts});
+    })
+
+  axios.get('https://jsonplaceholder.typicode.com/photos?_limit=30')
+    .then(res =>{
+      const blogImages = res.data;
+      this.setState({blogImages});
+    })
+
+  axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res =>{
+      const blogAuthors = res.data;
+      this.setState({blogAuthors});
+    }) 
+}
+
+render() {
+ 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+
+            <h2>My first React App using ReactJS & JSON</h2>
+            <p>By Masie</p>
+            <br></br>
+
+            <BlogPosts blogAuthors={this.state.blogAuthors} blogPosts={this.state.blogPosts} blogImages={this.state.blogImages} />
+        
+        </div> 
       </div>
     );
   }
+
 }
 
 export default App;
